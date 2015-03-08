70c9ae52093f4c52bac0370665c0d851b513710b
Tsurusaki <ka.trsk@gmail.com>
2015-03-08 14:06:35 +0900

    オリジナルapp作成

---
+++ new app/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  new app
+//
+//  Created by 鶴崎かんな on 2015/03/07.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import Foundation
--- new app/ViewController.swift
+++ new app/ViewController.swift
@@ -9,8 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var shinraiLabel:UILabel!
+     var shinrai:Int = 10
+    
+    @IBOutlet var powerLabel:UILabel!
+     var power:Int = 10
+    
+    @IBOutlet var syusaiLabel:UILabel!
+     var syusai:Int = 10
+    
+    @IBOutlet var sportsLabel:UILabel!
+     var sports:Int = 10
 
-    override func viewDidLoad() {
+    @IBOutlet var yearLabel:UILabel!
+     var year:Int = 6
+
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -19,7 +33,80 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func stady(){
+    shinrai = shinrai + 3
+    shinraiLabel.text = "\(shinrai)"
+    power = power - 3
+    powerLabel.text = "\(power)"
+    syusai = syusai + 5
+    syusaiLabel.text = "\(syusai)"
+    }
+    
+    @IBAction func book(){
+    shinrai = shinrai + 2
+    shinraiLabel.text = "\(shinrai)"
+    power = power - 2
+    powerLabel.text = "\(power)"
+    syusai = syusai + 3
+    syusaiLabel.text = "\(syusai)"
+    }
+    
+    @IBAction func soccer(){
+    sports = sports + 5
+    sportsLabel.text = "\(sports)"
+    power = power - 5
+    powerLabel.text = "\(power)"
+    }
+    
+    @IBAction func food(){
+    power = power + 10
+    powerLabel.text = "\(power)"
+    year = year + 1
+    yearLabel.text = "\(year)"
+    }
+    
+    @IBAction func help(){
+    shinrai = shinrai + 5
+    shinraiLabel.text = "\(shinrai)"
+    power = power - 5
+    powerLabel.text = "\(power)"
+    }
+    
+    @IBAction func fight(){
+    power = power - 10
+    powerLabel.text = "\(power)"
+    sports = sports + 10
+    sportsLabel.text = "\(sports)"
+    }
+    
+    @IBAction func kataduke(){
+    shinrai = shinrai + 5
+    shinraiLabel.text = "\(shinrai)"
+    power = power - 5
+    powerLabel.text = "\(power)"
+    }
+    
+    @IBAction func game(){
+    shinrai = shinrai - 3
+    shinraiLabel.text = "\(shinrai)"
+    power = power + 10
+    powerLabel.text = "\(power)"
+    syusai = syusai - 3
+    syusaiLabel.text = "\(syusai)"
+    }
+        
+    @IBAction func brake(){
+    power = power + 20
+    powerLabel.text = "\(power)"
+    sports = sports - 10
+    sportsLabel.text = "\(sports)"
+    year = year + 1
+    yearLabel.text = "\(year)"
+    }
 
 
-}
+
+    }
+    
+
 
